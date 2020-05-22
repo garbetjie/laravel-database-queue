544a8e7068ad9d064a99090378e8820528956636
Geoff Garbers <geoff@garbers.co.za>
2020-05-22 15:38:09 +0200

    Fix missing return value in service provider.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -18,6 +18,8 @@ class ServiceProvider extends BaseServiceProvider
                         return new Connector($this->app['db']);
                     }
                 );
+
+                return $queueManager;
             }
         );
     }
