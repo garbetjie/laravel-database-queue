6f24be7b1431baae37dac4ce6a5e2972458dbbed
Geoff Garbers <geoff@garbers.co.za>
2020-05-25 08:05:46 +0200

    Change name of migration class to describe what it is doing.

--- src/Console/Command/CreateMigration.php
+++ src/Console/Command/CreateMigration.php
@@ -2,23 +2,39 @@
 
 namespace Garbetjie\Laravel\DatabaseQueue\Console\Command;
 
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Queue\Console\TableCommand;
 use function str_replace;
 
 class CreateMigration extends TableCommand
 {
+    /**
+     * @var string
+     */
     protected $signature = 'garbetjie:queue:table';
 
+    /**
+     * @var string
+     */
     protected $description = 'Create the migration for enabling optimistic locking on the queue table.';
 
+    /**
+     * @param string $table
+     * @return string
+     */
     protected function createBaseMigration($table = 'jobs')
     {
         return $this->laravel['migration.creator']->create(
-            'add_version_to_'.$table.'_table', $this->laravel->databasePath().'/migrations'
+            'enable_optimistic_locking_on_'.$table.'_table', $this->laravel->databasePath().'/migrations'
         );
     }
 
-
+    /**
+     * @param string $path
+     * @param string $table
+     * @param string $tableClassName
+     * @throws FileNotFoundException
+     */
     protected function replaceMigration($path, $table, $tableClassName)
     {
         $stub = str_replace(
